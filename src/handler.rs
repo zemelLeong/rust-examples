7214c438fffa3250605040c66aa66fcf86c4c69c
zemel leong <zemel.leong@gmail.com>
2022-12-28 13:44:55 +0800

    Project init

---
+++ src/handler.rs
@@ -0,0 +1,99 @@
+use crate::protocol;
+use libp2p::swarm::{
+    ConnectionHandler, ConnectionHandlerEvent, ConnectionHandlerUpgrErr, KeepAlive,
+    SubstreamProtocol,
+};
+use std::collections::VecDeque;
+use std::task::{Context, Poll};
+
+/// The successful result of processing an inbound or outbound ping.
+#[derive(Debug)]
+pub enum Success {
+    OK,
+}
+
+pub struct Handler {
+    /// Outbound Inbound events
+    queued_events: VecDeque<
+        ConnectionHandlerEvent<
+            <Self as ConnectionHandler>::OutboundProtocol,
+            <Self as ConnectionHandler>::OutboundOpenInfo,
+            <Self as ConnectionHandler>::OutEvent,
+            <Self as ConnectionHandler>::Error,
+        >,
+    >,
+}
+
+impl Handler {
+    /// Builds a new `PingHandler` with the given configuration.
+    pub fn new() -> Self {
+        Handler {
+            queued_events: Default::default(),
+        }
+    }
+}
+
+impl ConnectionHandler for Handler {
+    type InEvent = protocol::MsgContent;
+    type OutEvent = protocol::MsgContent;
+    type Error = std::io::Error;
+    type InboundProtocol = protocol::MsgContent;
+    type OutboundProtocol = protocol::MsgContent;
+    type OutboundOpenInfo = ();
+    type InboundOpenInfo = ();
+
+    fn listen_protocol(&self) -> SubstreamProtocol<protocol::MsgContent, ()> {
+        SubstreamProtocol::new(
+            protocol::MsgContent {
+                data: Default::default(),
+            },
+            (),
+        )
+    }
+
+    //protocol::InboundUpgrade::Output
+    fn inject_fully_negotiated_inbound(&mut self, output: Vec<u8>, (): ()) {
+        //println!("inject_fully_negotiated_inbound ");
+
+        self.queued_events
+            .push_back(ConnectionHandlerEvent::Custom(protocol::MsgContent {
+                data: output,
+            }));
+    }
+
+    fn inject_fully_negotiated_outbound(&mut self, _output: protocol::Success, (): ()) {
+
+        //println!("inject_fully_negotiated_outbound");
+    }
+
+    fn inject_event(&mut self, msg: protocol::MsgContent) {
+        //println!("handler inject event ");
+        self.queued_events
+            .push_back(ConnectionHandlerEvent::OutboundSubstreamRequest {
+                protocol: SubstreamProtocol::new(msg, ()),
+            });
+    }
+
+    fn inject_dial_upgrade_error(
+        &mut self,
+        _info: (),
+        _error: ConnectionHandlerUpgrErr<std::io::Error>,
+    ) {
+    }
+
+    fn connection_keep_alive(&self) -> KeepAlive {
+        KeepAlive::Yes
+    }
+
+    fn poll(
+        &mut self,
+        _cx: &mut Context<'_>,
+    ) -> Poll<ConnectionHandlerEvent<protocol::MsgContent, (), protocol::MsgContent, Self::Error>>
+    {
+        if let Some(msg) = self.queued_events.pop_back() {
+            return Poll::Ready(msg);
+        }
+
+        Poll::Pending
+    }
+}
---
+++ src/protocol.rs
@@ -0,0 +1,71 @@
+use futures::future::BoxFuture;
+use futures::prelude::*;
+use libp2p::core::{upgrade, InboundUpgrade, OutboundUpgrade, UpgradeInfo};
+use libp2p::swarm::NegotiatedSubstream;
+use std::{io, iter};
+
+pub enum Success {
+    OK,
+}
+
+#[derive(Default, Debug, Clone)]
+pub struct MsgContent {
+    pub data: Vec<u8>,
+}
+
+impl UpgradeInfo for MsgContent {
+    type Info = &'static [u8];
+    type InfoIter = iter::Once<Self::Info>;
+
+    fn protocol_info(&self) -> Self::InfoIter {
+        iter::once(b"/p2p/msg/1.0.0")
+    }
+}
+
+impl InboundUpgrade<NegotiatedSubstream> for MsgContent {
+    type Output = Vec<u8>;
+    type Error = std::io::Error;
+    type Future = BoxFuture<'static, Result<Self::Output, Self::Error>>;
+
+    fn upgrade_inbound(self, socket: NegotiatedSubstream, _: Self::Info) -> Self::Future {
+        //println!("upgrade_inbound");
+        async move {
+            let packet = recv(socket).await?;
+            Ok(packet)
+        }
+        .boxed()
+    }
+}
+
+impl OutboundUpgrade<NegotiatedSubstream> for MsgContent {
+    type Output = Success;
+    type Error = std::io::Error;
+    type Future = BoxFuture<'static, Result<Self::Output, Self::Error>>;
+    fn upgrade_outbound(self, socket: NegotiatedSubstream, _: Self::Info) -> Self::Future {
+        //println!("upgrade_outbound {:?}",self.data);
+        async move {
+            send(socket, self.data).await?;
+            Ok(Success::OK)
+        }
+        .boxed()
+    }
+}
+
+pub async fn recv<S>(mut socket: S) -> io::Result<Vec<u8>>
+where
+    S: AsyncRead + AsyncWrite + Unpin,
+{
+    let packet = upgrade::read_length_prefixed(&mut socket, 2048).await?;
+    //println!("{:?}",std::str::from_utf8(&packet).unwrap());
+
+    Ok(packet)
+}
+
+pub async fn send<S>(mut socket: S, data: Vec<u8>) -> io::Result<S>
+where
+    S: AsyncRead + AsyncWrite + Unpin,
+{
+    upgrade::write_length_prefixed(&mut socket, data).await?;
+    socket.close().await?;
+    Ok(socket)
+}
